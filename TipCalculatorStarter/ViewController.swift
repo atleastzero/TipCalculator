2a67af91656be8e665e8e9bd44efcb5bfbd2e192
atleastzero <megan.obryan52@gmail.com>
2020-02-06 20:17:08 -0800

    Some styling.

--- TipCalculatorStarter/ViewController.swift
+++ TipCalculatorStarter/ViewController.swift
@@ -31,6 +31,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupViews()
 
         if self.billAmountTextField.isFirstResponder {
             self.billAmountTextField.resignFirstResponder()
@@ -92,6 +93,25 @@ class ViewController: UIViewController {
         tipPercentSegmentedControl.selectedSegmentIndex = 0
         tipAmountLabel.text = "$0.00"
         totalAmountLabel.text = "$0.00"
+    }
+    
+    func setupViews() {
+        headerView.layer.shadowOffset = CGSize(width: 0, height: 1)
+        headerView.layer.shadowOpacity = 0.05
+        headerView.layer.shadowColor = UIColor.black.cgColor
+        headerView.layer.shadowRadius = 35
+
+        inputCardView.layer.cornerRadius = 8
+        inputCardView.layer.masksToBounds = true
+
+        outputCardView.layer.cornerRadius = 8
+        outputCardView.layer.masksToBounds = true
+
+        outputCardView.layer.borderWidth = 1
+        outputCardView.layer.borderColor = UIColor.tcHotPink.cgColor
+
+        resetButton.layer.cornerRadius = 8
+        resetButton.layer.masksToBounds = true
     }
 }
 
