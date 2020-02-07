c32bd6cbf7df62cbf77afefda0a17dbe37908c05
atleastzero <megan.obryan52@gmail.com>
2020-02-06 19:46:36 -0800

    Finished page 5.

--- TipCalculatorStarter/ViewController.swift
+++ TipCalculatorStarter/ViewController.swift
@@ -14,6 +14,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var themeSwitch: UISwitch!
     
+    @IBOutlet weak var inputCardView: UIView!
+    @IBOutlet weak var billAmountTextField: UIStackView!
+    @IBOutlet weak var tipPercentSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var outputCardView: UIView!
+    @IBOutlet weak var tipAmountTitleLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountTitleLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
@@ -21,6 +33,10 @@ class ViewController: UIViewController {
     }
     
     @IBAction func themeToggled(_ sender: UISwitch) {
+    }
+    @IBAction func tipPercentChanged(_ sender: Any) {
+    }
+    @IBAction func resetButtonTapped(_ sender: Any) {
     }
 }
 
