e5fc8202c83a0c4d96ad9fec3d18558a0055bea6
atleastzero <megan.obryan52@gmail.com>
2020-02-06 19:16:15 -0800

    Initial commit

---
+++ TipCalculatorStarter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TipCalculatorStarter
+//
+//  Created by Chase Wang on 9/19/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func themeToggled(_ sender: UISwitch) {
+    }
+}
+
